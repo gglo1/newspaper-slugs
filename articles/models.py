b08fd38f98ab8f8dfc4e8e636f60de39c4b90ae3
gglo1 <logaozong@gmail.com>
2023-11-19 09:58:01 -0900

    Add files via upload

---
+++ articles/models.py
@@ -0,0 +1,20 @@
+# articles/models.py
+from django.db import models
+from django.template.defaultfilters import slugify  # new
+from django.urls import reverse
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    slug = models.SlugField(null=False, unique=True) #new
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("article_detail", kwargs={"slug": self.slug})
+
+    def save(self, *args, **kwargs):  # new
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
---
+++ articles/urls.py
@@ -0,0 +1,9 @@
+# articles/urls.py
+from django.urls import path
+
+from .views import ArticleListView, ArticleDetailView
+
+urlpatterns = [
+    path("<slug:slug>", ArticleDetailView.as_view(), name="article_detail"),  # new
+    path("", ArticleListView.as_view(), name="article_list"),
+]
